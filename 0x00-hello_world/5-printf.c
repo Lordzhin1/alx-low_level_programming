051072141566319d00cb0710afc6abb1d9e703f9
Lordzhin1 <isaacaniapa32@gmail.com>
2023-02-17 13:12:06 -0800

    task5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,9 @@
-#!/bin/bash
 #include <stdio.h>
- /**
+/**
  * main - Entry point
  *
  * Return: Always 0 (success)
- * /
+ */
 int main(void)
 {
 	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
